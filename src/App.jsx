1d6d61e7028adb9fd6a064a8ef0a531b8437542b
Chukroid <kossyemmanuel20@gmail.com>
2024-07-01 20:15:50 -0600

    created layout with pure html

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,29 @@
+import TextBox from "./components/TodoTextbox";
+
 export default function App(){
-  return "hi"
+  
+
+  return (
+    <>
+    <h1>Your Todo List</h1>
+    <TextBox/>
+    <ul>
+      <li>
+        <label>TODO Name</label>
+        <input type="checkbox"/>
+        <button>Delete</button>
+      </li>
+      <li>
+        <label>TODO Name</label>
+        <input type="checkbox"/>
+        <button>Delete</button>
+      </li>
+      <li>
+        <label>TODO Name</label>
+        <input type="checkbox"/>
+        <button>Delete</button>
+      </li>
+    </ul>
+    </>
+  )
 }
