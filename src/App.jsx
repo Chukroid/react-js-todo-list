336276d1e5093b0359d5e1b8f513cdd6812c817e
Chukroid <kossyemmanuel20@gmail.com>
2024-07-01 20:40:42 -0600

    textbox works

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
 import TextBox from "./components/TodoTextbox";
 
 export default function App(){
-  
+  const [todolist,setNewItem] = useState(() => {
+    const storagevalue = localStorage.getItem("Todos")
+    if (storagevalue == null) return []
+    return JSON.parse(storagevalue)
+  }) //gets the todolist which is the input text and the setnewitem function to be called to set it
+
+
+  useEffect(() => { // anytime the todolist updates, it should automatically update the local storage 
+    localStorage.setItem("Todos", JSON.stringify(todolist))
+  },[todolist])// the second argument is an array of all the objects we want the useeffect to detect changes on
+
 
   return (
     <>
--- src/components/TodoTextbox.jsx
+++ src/components/TodoTextbox.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react"
+
 export default function TodoTextBox(){
+    const [newitem,setNewItem] = useState("") //gets the newitem which is the input text and the setnewitem function to be called to set the value of newitem to the new one
+
     return(
         <>
         <form >
             <label htmlFor="item">Add new Todo</label>
-            <input type="text" id="item" />
+            {/*setting the value of the textbox to newitem and setting its change event to the setnewitem function */}
+            <input type="text" id="item" value={newitem} onChange={e => setNewItem(e.target.value)}/>
             <button>ADD</button>
         </form>
         </>
