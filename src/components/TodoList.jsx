f54bf799e6e2dfbc7a1e9f03a8f72928a0be2305
Chukroid <kossyemmanuel20@gmail.com>
2024-07-11 00:50:09 -0600

    added a text that indicates that there are no todos when todo list is empty

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,19 @@
 import TodoListCategory from "./TodoListCategory"
 
 export default function TodoList({ todos, allTags, ...rest }){
-    return (
-        <>
-        {allTags.map(tag => {
-            return (
-                <TodoListCategory tagvalue={tag} key={tag.value} todos={todos} restf={rest} />
-            )
-        })}
-        </>
-    )
+
+    if (todos.length === 0){
+        return <p className="empty-text">You currently have no Todos To do.</p>
+    }else{
+        return (
+            <>
+            {todos.length === 0 && ""}
+            {allTags.map(tag => {
+                return (
+                    <TodoListCategory tagvalue={tag} key={tag.value} todos={todos} restf={rest} />
+                )
+            })}
+            </>
+        )
+    }
 }
