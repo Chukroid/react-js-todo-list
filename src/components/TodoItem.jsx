33830d7e3b0a8dc7a9094a5346fc98784288d819
Chukroid <kossyemmanuel20@gmail.com>
2024-07-12 09:11:05 -0600

    created a custom checkbox and added some transitions to all the components of the todo-item

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,13 @@
 export default function TodoItem({ todoitem, otherfuncs }){
+    const checkboxid = "checkbox-"+todoitem.id
+
     return (
         <>
         <li key={todoitem.id} className="todo-item"> {/* every iteration must have a key */}
             <div className="item-info">
-                <input className="todo-completed" type="checkbox" checked={todoitem.completed || false} onChange={e => otherfuncs.toggletodo(todoitem.id,e.target.checked)}/>
-                <label className={todoitem.completed && "striked" || ""}>{todoitem.title || "Invalid Title"}</label>
+                <input id={checkboxid} type="checkbox" checked={todoitem.completed || false} onChange={e => otherfuncs.toggletodo(todoitem.id,e.target.checked)}/>
+                <label className="todo-completed" htmlFor={checkboxid}><i className="bi bi-check"></i>                </label>
+                <label className={"todo-title" + (todoitem.completed && " striked" || "")}>{todoitem.title || "Invalid Title"}</label>
                 <button className="todo-delete"
                     onClick={() => otherfuncs.deletetodo(todoitem.id)}
                 ><i className="bi bi-trash-fill"></i></button>
