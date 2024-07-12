824d44fb7c212ad294608403b2a717572c1a1166
Chukroid <kossyemmanuel20@gmail.com>
2024-07-12 09:50:10 -0600

    added transitions to other components of the todo app, I also made it that when the tittle of the todo item is clicked, it marks it as complete or viceversa aswell

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -7,7 +7,7 @@ export default function TodoItem({ todoitem, otherfuncs }){
             <div className="item-info">
                 <input id={checkboxid} type="checkbox" checked={todoitem.completed || false} onChange={e => otherfuncs.toggletodo(todoitem.id,e.target.checked)}/>
                 <label className="todo-completed" htmlFor={checkboxid}><i className="bi bi-check"></i>                </label>
-                <label className={"todo-title" + (todoitem.completed && " striked" || "")}>{todoitem.title || "Invalid Title"}</label>
+                <label className={"todo-title" + (todoitem.completed && " striked" || "")} htmlFor={checkboxid}>{todoitem.title || "Invalid Title"}</label>
                 <button className="todo-delete"
                     onClick={() => otherfuncs.deletetodo(todoitem.id)}
                 ><i className="bi bi-trash-fill"></i></button>
